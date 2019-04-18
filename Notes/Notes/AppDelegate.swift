01d4eeb32f2d138d3a58742ec10bd086f18397d3
Anthony <42779104+anthonymione@users.noreply.github.com>
2019-04-18 15:23:44 -0600

    Added Notes app

---
+++ Notes/Notes/AppDelegate.swift
@@ -0,0 +1,87 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Irina on 7/30/17.
+//  Copyright © 2017 Apple Developer. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        UINavigationBar.appearance().barTintColor = UIColor(red: 245.0/255.0, green: 79.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 242.0/255.0, alpha: 1.0)
+        let color = UIColor(red: 242.0/255.0, green: 242.0/255.0, blue: 242.0/255.0, alpha: 1.0)
+        let font = UIFont(name: "Roboto-Medium", size: 18)!
+        
+        let attributes: [NSAttributedStringKey: AnyObject] = [
+            NSAttributedStringKey(rawValue: NSAttributedStringKey.foregroundColor.rawValue): color,
+            NSAttributedStringKey(rawValue: NSAttributedStringKey.font.rawValue): font
+        ]
+        
+        UINavigationBar.appearance().titleTextAttributes = attributes
+        
+        UIApplication.shared.statusBarStyle = .lightContent
+
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        self.saveContext()
+        
+    }
+
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "Notes")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let context = appDelegate.persistentContainer.viewContext
